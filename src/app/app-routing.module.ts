f5f58e09b69f3bf2f85d29bb9fd94a0569606b5a
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-10-03 16:16:25 -0500

    creation of design base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationSelectorComponent } from './components/location-selector/location-selector.component';
+import { ChildrenComponent } from './components/children/children.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/children', pathMatch: 'full' },
+  { 
+    path: 'children', 
+    component: ChildrenComponent,
+    children: [
+      { path: '', component: LocationSelectorComponent },
+      { path: 'location-selector', component: LocationSelectorComponent },
+    ]
+  },
+  { path: '**', redirectTo: '/children' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
